ccf1ed5535fd2be3ed8cc111c74b4bdb033432aa
Manal <manoula8987@gmail.com>
2024-10-26 18:37:14 +0100

    Update RecipeDisplay.js

--- src/RecipeDisplay.js
+++ src/RecipeDisplay.js
@@ -3,14 +3,14 @@ import "./style.css";
 
 export default function RecipeDisplay({ recipe }) {
   return (
-    recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+    recipe != null && (
       <div
         className="recipeTile"
-        onClick={() => window.open(recipe["recipe"]["url"])}//sets an onClick event handler, which will open the recipe's URL in a new window when the tile is clicked.
+        onClick={() => window.open(recipe["url"])}//sets an onClick event handler, which will open the recipe's URL in a new window when the tile is clicked.
       >
-        <img className="recipeTile__img" src={recipe["recipe"]["image"]} alt={recipe["recipe"]["label"]} />
+        <img className="recipeTile__img" src={recipe["image"]} alt={recipe["label"]} />
         <p className="recipeTile__name">
-          {recipe["recipe"]["label"]}
+          {recipe["label"]}
         </p>
       </div>
     )
